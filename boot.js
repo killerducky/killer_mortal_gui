3d290aa317d601ddd6be08d97fef69641b3862c2
Andy Olsen <andy.olsen.tx@gmail.com>
2024-07-07 05:50:53 -0500

    rev d=10

--- boot.js
+++ boot.js
@@ -1,4 +1,4 @@
 // For debug, create window.MM so we can do debug from dev console
 // e.g. MM.debugState()
-import mainModule from "./index.js?d=9"
+import mainModule from "./index.js?d=10"
 window.MM = mainModule
